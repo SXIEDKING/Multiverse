77b87646989bd6923d581d6ae5630427fd1a1fa4
SXIEDKING <saiedislam61@gmail.com>
2022-04-26 16:33:29 +0100

    Class Passenger

---
+++ script.js
@@ -0,0 +1,46 @@
+"use strict"
+
+class Bag {
+    
+    constructor(weight) {
+        this.weight = weight
+    }
+    isOverLimit(){
+        if(weight >= 20){
+            return true;
+        } else{return false;}
+            
+        
+    }
+    
+}
+
+
+try{
+       const bag = new Bag(16)
+} catch {
+    console.log(error)
+}
+
+class Passenger{
+    bags = [];
+    constructor(Name,PassportNumber,SeatNumber){
+        Name = "";
+        PassportNumber = "";
+        SeatNumber = "";
+    }
+    
+if (_travellersCabinBag.isOverLimit)  {
+
+}
+
+}    
+
+const traveller = new Passenger('Bob the Builder', "Passport123", "1A");
+const travellersCabinBag = new Bag(9);
+const travellersHullBag = new Bag(23);
+traveller.addBag(travellersCabinBag);
+traveller.addBag(travellersHullBag);
+
+
+    
